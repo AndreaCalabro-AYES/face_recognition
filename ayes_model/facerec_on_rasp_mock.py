5fff97d3d7b9af547cd8b2db195ccf0fddea28eb
AndreaCalabro-AYES <andrea.calabro@ayesconsulting.com>
2024-07-02 14:28:30 +0200

    WIP: mock up of the communication with the broker

--- ayes_model/facerec_on_rasp_mock.py
+++ ayes_model/facerec_on_rasp_mock.py
@@ -8,10 +8,8 @@ import face_recognition
 import numpy as np
 import cv2
 import time
+import paho.mqtt.client as mqtt
 
-import sys
-sys.path.insert(0, '../MQTT_test/Listener_Publisher')
-import jolly as mqtt_client
 
 
 def load_db(db_path):
@@ -64,8 +62,82 @@ names = [encoding['name'] for encoding in db]
 known_encodings = [np.array(encoding['encoding']) for encoding in db] 
 
 print("Encodings have been loaded", flush=True)
+MQTT_TOPICS = ["greetings/face_added",
+               "greetings/face_removed"]
 
-mqtt_client.connect()
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
+CLIENT_ID = "FaceRecognition"
+MQTT_BROKER_HOST = "mqtt_broker"
+MQTT_BROKER_PORT = 1883
+
+def on_connect(client, userdata, flags, rc, properties):
+    if rc == 0:
+        print(f"Connected to broker at {MQTT_BROKER_HOST}: {MQTT_BROKER_PORT}", flush=True)
+        for mqtt_topic in MQTT_TOPICS:
+            client.subscribe(mqtt_topic)
+    else:
+        print("Failed to connect, return code %d", rc, flush=True)
+
+def on_subscribe(client, userdata, mid, reason_code_list, properties):
+    if reason_code_list[0].is_failure:
+        print(f"Broker rejected you subscription: {reason_code_list[0]}", flush=True)
+    else:
+        print(f"Broker granted the following QoS: {reason_code_list[0].value}", flush=True)
+
+def on_message(client, userdata, msg):
+    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}", flush=True)
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code: %s", rc, flush=True)
+    reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        print("Reconnecting in %d seconds...", reconnect_delay, flush=True)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print("Reconnected successfully!", flush=True)
+            return
+        except Exception as err:
+            print("%s. Reconnect failed. Retrying...", err, flush=True)
+
+        reconnect_delay *= RECONNECT_RATE
+        reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+        reconnect_count += 1
+    print("Reconnect failed after %s attempts. Exiting...", reconnect_count, flush=True)
+
+# Set Connecting Client ID
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=CLIENT_ID)
+
+# client.username_pw_set(username, password)
+mqtt_client.on_connect = on_connect
+mqtt_client.on_subscribe = on_subscribe
+mqtt_client.on_message = on_message
+mqtt_client.on_disconnect = on_disconnect
+
+def connect():
+    mqtt_client.connect(
+        host = MQTT_BROKER_HOST,
+        port = MQTT_BROKER_PORT,
+        keepalive = 60
+    )
+    print(f"Connected to broker at {MQTT_BROKER_HOST}: {MQTT_BROKER_PORT}", flush=True)
+
+    mqtt_client.loop_start()
+
+def publish(topic, message):
+    result = mqtt_client.publish(topic, message)
+    status = result[0]
+    if status == 0:
+        print(f"Send `{message}` to topic `{topic}`", flush=True)
+    else:
+        print(f"Failed to send message to topic {topic}", flush=True)
+
+connect()
+
 
 # Initialize some variables
 face_locations = []
