f9684c5801a2b2b5e4ff0a0343d2c9f1d3ff2a7e
AndreaCalabro-AYES <andrea.calabro@ayesconsulting.com>
2024-07-10 15:55:37 +0200

    Working instance of ayes mqtt class, yet to try with the raspberry!

--- ayes_model/facerec_on_rasp_mock.py
+++ ayes_model/facerec_on_rasp_mock.py
@@ -8,7 +8,7 @@ import face_recognition
 import numpy as np
 import cv2
 import time
-import paho.mqtt.client as mqtt
+from mqtt_client_ayes import AyesMqttClient
 
 
 
@@ -74,70 +74,12 @@ CLIENT_ID = "FaceRecognition"
 MQTT_BROKER_HOST = "mqtt_broker"
 MQTT_BROKER_PORT = 1883
 
-def on_connect(client, userdata, flags, rc, properties):
-    if rc == 0:
-        print(f"Connected to broker at {MQTT_BROKER_HOST}: {MQTT_BROKER_PORT}")
-        for mqtt_topic in MQTT_TOPICS:
-            client.subscribe(mqtt_topic)
-    else:
-        print("Failed to connect, return code %d", rc)
-
-def on_subscribe(client, userdata, mid, reason_code_list, properties):
-    if reason_code_list[0].is_failure:
-        print(f"Broker rejected you subscription: {reason_code_list[0]}")
-    else:
-        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
-
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
-
-def on_disconnect(client, userdata, rc):
-    print("Disconnected with result code: %s", rc)
-    reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
-    while reconnect_count < MAX_RECONNECT_COUNT:
-        print("Reconnecting in %d seconds...", reconnect_delay)
-        time.sleep(reconnect_delay)
-
-        try:
-            client.reconnect()
-            print("Reconnected successfully!")
-            return
-        except Exception as err:
-            print("%s. Reconnect failed. Retrying...", err)
-
-        reconnect_delay *= RECONNECT_RATE
-        reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
-        reconnect_count += 1
-    print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
-
-# Set Connecting Client ID
-mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=CLIENT_ID)
-
-# client.username_pw_set(username, password)
-mqtt_client.on_connect = on_connect
-mqtt_client.on_subscribe = on_subscribe
-mqtt_client.on_message = on_message
-mqtt_client.on_disconnect = on_disconnect
-
-def connect():
-    mqtt_client.connect(
-        host = MQTT_BROKER_HOST,
-        port = MQTT_BROKER_PORT,
-        keepalive = 60
-    )
-    print(f"Connected to broker at {MQTT_BROKER_HOST}: {MQTT_BROKER_PORT}")
-
-    mqtt_client.loop_start()
-
-def publish(topic, message):
-    result = mqtt_client.publish(topic, message)
-    status = result[0]
-    if status == 0:
-        print(f"Send `{message}` to topic `{topic}`")
-    else:
-        print(f"Failed to send message to topic {topic}")
-
-connect()
+face_recognition_client = AyesMqttClient(
+    broker= MQTT_BROKER_HOST,
+    port= MQTT_BROKER_PORT,
+    topics_list= MQTT_TOPICS,
+    client_id= CLIENT_ID
+)
 
 
 # Initialize some variables
@@ -158,6 +100,7 @@ print("Ready to start recognition")
 
 time.sleep(2)
 
+face_recognition_client.connect()
 
 while True:
 
@@ -257,9 +200,9 @@ while True:
         if publish_flag:
             face_added_names = list(set(face_added_names))
             face_added = json.dumps({"names" : face_added_names})
-            mqtt_client.publish("greetings/face_added", face_added)
+            face_recognition_client.publish_message("greetings/face_added", face_added)
             face_removed = json.dumps({"names" : face_removed_names})
-            mqtt_client.publish("greetings/face_removed", face_removed)
+            face_recognition_client.publish_message("greetings/face_removed", face_removed)
             
 
         prev_faces_nb = len(face_locations)
